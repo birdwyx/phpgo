dfee0062303398718e69c709ae1f4ec9be2ef84f
michaelwei <michaelwei@bp1010.com>
2018-08-05 23:17:38 +0800

    test

--- src/phpgo_context.h
+++ src/phpgo_context.h
@@ -217,6 +217,7 @@ struct PhpgoBaseContext{
 
 	struct _zend_execute_data* EG_current_execute_data;
 	void*                      EG_bailout;
+	char                       buffer[128];
 #if PHP_MAJOR_VERSION < 7
 	/*go routine running environment*/
 	zend_vm_stack 			   EG_argument_stack;       
