9e08fd556af19217d23e60de7a8dce6b6699f56c
michaelwei@bp1010.com <michaelwei@bp1010.com>
2019-03-21 01:38:42 +0800

    php5.4/5.5 to call shutdown func -3

--- src/basic_functions.cc
+++ src/basic_functions.cc
@@ -53,18 +53,6 @@ static int user_shutdown_function_call(php_shutdown_function_entry *shutdown_fun
 }
 /* }}} */
 
-void php_call_shutdown_functions(TSRMLS_D) /* {{{ */
-{
-	if (BG(user_shutdown_function_names)) {
-		zend_try {
-			zend_hash_apply(BG(user_shutdown_function_names), (apply_func_t) user_shutdown_function_call TSRMLS_CC);
-		}
-		zend_end_try();
-		php_free_shutdown_functions(TSRMLS_C);
-	}
-}
-/* }}} */
-
 void php_free_shutdown_functions(TSRMLS_D) /* {{{ */
 {
 	if (BG(user_shutdown_function_names))
@@ -77,6 +65,18 @@ void php_free_shutdown_functions(TSRMLS_D) /* {{{ */
 			FREE_HASHTABLE(BG(user_shutdown_function_names));
 			BG(user_shutdown_function_names) = NULL;
 		} zend_end_try();
+}
+/* }}} */
+
+void php_call_shutdown_functions(TSRMLS_D) /* {{{ */
+{
+	if (BG(user_shutdown_function_names)) {
+		zend_try {
+			zend_hash_apply(BG(user_shutdown_function_names), (apply_func_t) user_shutdown_function_call TSRMLS_CC);
+		}
+		zend_end_try();
+		php_free_shutdown_functions(TSRMLS_C);
+	}
 }
 /* }}} */
 
